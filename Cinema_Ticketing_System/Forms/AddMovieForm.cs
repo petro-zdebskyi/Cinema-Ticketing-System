69a95abf01211c0f00a7bb17d51e7373af7d9c3e
ukrainian1patriot <petroZd@gmail.com>
2016-03-25 12:45:44 +0200

    Update AddMovieForm.cs

--- Cinema_Ticketing_System/Forms/AddMovieForm.cs
+++ Cinema_Ticketing_System/Forms/AddMovieForm.cs
@@ -25,6 +25,7 @@ namespace Cinema_Ticketing_System
         private void addMovieButton_Click(object sender, EventArgs e)
         {
             _movie.Name = movieNameTextBox.Text;
+            // Petro Zdebsky Review: use Int32.TryParse() instead Convert.ToInt32()
             _movie.RunningTime = Convert.ToInt32(runningLengthTextBox.Text);
             _movie.Description = descriptionTextBox.Text;
             DialogResult = DialogResult.OK;
